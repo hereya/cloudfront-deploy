c23ef6fcf9a418135f5bee838892c8cabd837f59
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-05-27 00:44:05 +0200

    Add support for custom domain and SSL certificate
    Added new features to the cloudfront deployment stack. A custom domain can now be associated with the cloudfront distribution, and a SSL certificate can also be attached. Also enabled the handling of cross region references in the CloudfrontDeployStack instantiation.

--- bin/cloudfront-deploy.ts
+++ bin/cloudfront-deploy.ts
@@ -4,4 +4,6 @@ import * as cdk from 'aws-cdk-lib';
 import { CloudfrontDeployStack } from '../lib/cloudfront-deploy-stack';
 
 const app = new cdk.App();
-new CloudfrontDeployStack(app, process.env.STACK_NAME!)
+new CloudfrontDeployStack(app, process.env.STACK_NAME!, {
+    crossRegionReferences: true,
+})
--- lib/cloudfront-deploy-stack.ts
+++ lib/cloudfront-deploy-stack.ts
@@ -6,6 +6,9 @@ import { Bucket, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
 import * as path from 'node:path';
 import { Distribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { CertificateValidation, DnsValidatedCertificate, ICertificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { ARecord, HostedZone, IHostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
 
 export class CloudfrontDeployStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -17,11 +20,33 @@ export class CloudfrontDeployStack extends cdk.Stack {
             removalPolicy: cdk.RemovalPolicy.DESTROY,
         })
 
-        const hereyaProjectRootDir: string = this.node.tryGetContext('hereyaProjectRootDir');
         const distFolder: string = this.node.tryGetContext('distFolder') ?? 'dist';
+        const hereyaProjectRootDir: string = this.node.tryGetContext('hereyaProjectRootDir');
         if(!hereyaProjectRootDir) {
             throw new Error('hereyaProjectRootDir context variable is required');
         }
+
+        const customDomain = this.node.tryGetContext('customDomain');
+        const domainZone = this.node.tryGetContext('domainZone');
+        if(customDomain && !domainZone) {
+            throw new Error('domainZone context variable is required when using customDomain');
+        }
+
+        let certificate: ICertificate | undefined;
+        let hostedZone: IHostedZone | undefined;
+        if(customDomain) {
+            hostedZone = HostedZone.fromLookup(this, 'HostedZone', {
+                domainName: domainZone,
+            })
+
+            certificate = new DnsValidatedCertificate(this, 'Certificate', {
+                domainName: customDomain,
+                hostedZone,
+                region: 'us-east-1',
+                validation: CertificateValidation.fromDns(hostedZone),
+            })
+        }
+
 
         new BucketDeployment(this, 'BucketDeployment', {
             destinationBucket: bucket,
@@ -36,15 +61,30 @@ export class CloudfrontDeployStack extends cdk.Stack {
             defaultBehavior: {
                 origin: new S3Origin(bucket, {originAccessIdentity}),
             },
+            domainNames: customDomain ? [customDomain] : undefined,
+            certificate: certificate,
         })
+
+        if( customDomain && hostedZone ) {
+            new ARecord(this, 'AliasRecord', {
+                zone: hostedZone,
+                target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+            })
+        }
+
 
         new CfnOutput(this, 'BucketName', {
             value: bucket.bucketName,
         })
 
-        new CfnOutput(this, 'DistributionDomainName', {
-            value: distribution.distributionDomainName,
-        })
-
+        if(customDomain) {
+            new CfnOutput(this, 'DomainName', {
+                value: customDomain,
+            })
+        } else {
+            new CfnOutput(this, 'DistributionDomainName', {
+                value: distribution.distributionDomainName,
+            })
+        }
     }
 }
