79de7e1380d97e32f8a53d26dd93dc2872b47357
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-05-27 00:51:46 +0200

    Add environment variables to CloudfrontDeployStack
    Environment variables for the AWS account and region are now added to the CloudfrontDeployStack. This allows for better adaptability and region-specific adjustments if required.

--- bin/cloudfront-deploy.ts
+++ bin/cloudfront-deploy.ts
@@ -6,4 +6,8 @@ import { CloudfrontDeployStack } from '../lib/cloudfront-deploy-stack';
 const app = new cdk.App();
 new CloudfrontDeployStack(app, process.env.STACK_NAME!, {
     crossRegionReferences: true,
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
 })
